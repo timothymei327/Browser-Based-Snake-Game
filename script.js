1a376115cb02123551b72b1d967dbe49a5fb9a15
Timothy Mei <timothymei3270@gmail.com>
2022-07-06 13:20:38 -0400

    added broken movement to all directions

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let gameBoard = document.getElementById('container')
 const boardWidth = 20
 const boardHeight = 20
 let snakeBlocks = [183, 184, 185]
+let speed = 1000
+let direction = null
+let previousInput = ['gamestart']
 // let gameSession = true
 
 for (i = 0; i < 400; i++) {
@@ -42,22 +45,88 @@ const arrowPressed = (e) => {
   switch (e.keyCode) {
     case 37:
       console.log('left arrowkey pressed')
-      snakePosition.style.gridColumnStart -= 1
-      console.log(snakePosition.style.gridColumnStart)
+      direction = 'left'
+      const leftMovement = () => {
+        for (i = 0; i < snakeBlocks.length; i++) {
+          snakeBlocks.push(snakeBlocks[snakeBlocks.length - 1] - 1)
+          spaces[snakeBlocks[0]].setAttribute('id', 'snake')
+          spaces[snakeBlocks[0]].style.backgroundColor = '#465947'
+          snakeBlocks.shift(snakeBlocks[0])
+          previousInput.push('left')
+          snake()
+          setTimeout(leftMovement, speed)
+        }
+      }
+      if (
+        previousInput[previousInput.length - 1] !== 'left' &&
+        previousInput[previousInput.length - 1] !== 'right' &&
+        previousInput[previousInput.length - 1] !== 'gamestart'
+      ) {
+        leftMovement()
+      }
       break
     case 38:
       console.log('top arrowkey pressed')
-      snakePosition.style.gridRowStart -= 1
+      direction = 'top'
+      const topMovement = () => {
+        for (i = 0; i < snakeBlocks.length; i++) {
+          snakeBlocks.push(snakeBlocks[snakeBlocks.length - 1] - 20)
+          spaces[snakeBlocks[0]].setAttribute('id', 'snake')
+          spaces[snakeBlocks[0]].style.backgroundColor = '#465947'
+          snakeBlocks.shift(snakeBlocks[0])
+          previousInput.push('top')
+          snake()
+          setTimeout(topMovement, speed)
+        }
+      }
+      if (
+        previousInput[previousInput.length - 1] !== 'top' &&
+        previousInput[previousInput.length - 1] !== 'bottom'
+      ) {
+        topMovement()
+      }
       break
     case 39:
       console.log('right arrowkey pressed')
-      snakePosition.style.gridColumnStart =
-        parseInt(snakePosition.style.gridColumnStart) + parseInt(1)
+      direction = 'right'
+      const rightMovement = () => {
+        for (i = 0; i < snakeBlocks.length; i++) {
+          snakeBlocks.push(snakeBlocks[snakeBlocks.length - 1] + 1)
+          spaces[snakeBlocks[0]].setAttribute('id', 'snake')
+          spaces[snakeBlocks[0]].style.backgroundColor = '#465947'
+          snakeBlocks.shift(snakeBlocks[0])
+          previousInput.push('right')
+          snake()
+          setTimeout(rightMovement, speed)
+        }
+      }
+      if (
+        previousInput[previousInput.length - 1] !== 'right' &&
+        previousInput[previousInput.length - 1] !== 'left'
+      ) {
+        rightMovement()
+      }
       break
     case 40:
       console.log('bottom arrowkey pressed')
-      snakePosition.style.gridRowStart =
-        parseInt(snakePosition.style.gridRowStart) + parseInt(1)
+      direction = 'bottom'
+      const bottomMovement = () => {
+        for (i = 0; i < snakeBlocks.length; i++) {
+          snakeBlocks.push(snakeBlocks[snakeBlocks.length - 1] + 20)
+          spaces[snakeBlocks[0]].setAttribute('id', 'snake')
+          spaces[snakeBlocks[0]].style.backgroundColor = '#465947'
+          snakeBlocks.shift(snakeBlocks[0])
+          previousInput.push('bottom')
+          snake()
+          setTimeout(bottomMovement, speed)
+        }
+      }
+      if (
+        previousInput[previousInput.length - 1] !== 'bottom' &&
+        previousInput[previousInput.length - 1] !== 'top'
+      ) {
+        bottomMovement()
+      }
       break
   }
 }

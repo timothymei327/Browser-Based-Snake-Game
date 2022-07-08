e5425f800b69ec9839ee097861754d1b32294190
Timothy Mei <timothymei3270@gmail.com>
2022-07-07 23:25:28 -0400

    improved generateDots, refactored unused code

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ const boardWidth = 20
 const boardHeight = 20
 let snakeBlocks = [183, 184, 185]
 let dotBlocks = []
-let speed = 650
+let speed = 600
 let direction = null
 let previousInput = ['gamestart']
-let intervalTime = 250
+let intervalTime = 300
 let dotSpawnTime = 1
-let stopGame = true
+let numsArr = []
 
 for (i = 0; i < 400; i++) {
   let spaces = document.createElement('div')
@@ -24,18 +24,26 @@ const snake = () => {
     let snakeBody = spaces[snakeBlocks[i]]
     snakeBody.setAttribute('id', 'snake')
     snakeBody.style.backgroundColor = '#f46d10'
+    snakeBody.style.border = '1px solid #aa1408'
   }
 }
 
 snake()
 
+const randomNum = () => {
+  let nums = Math.round(Math.random() * 400)
+  numsArr.unshift(nums)
+}
+
 const generateDots = () => {
-  let randomNum = Math.round(Math.random() * 400)
-  if (snakeBlocks.includes(randomNum)) {
-    generateDots()
+  randomNum()
+  while (snakeBlocks.includes(numsArr[0])) {
+    numsArr.shift()
+    randomNum()
+    if (!snakeBlocks.includes(numsArr[0])) break
   }
-  let randomDots = spaces[randomNum]
-  dotBlocks.push(randomNum)
+  let randomDots = spaces[numsArr[0]]
+  dotBlocks.push(numsArr[0])
   if (snakeBlocks[snakeBlocks.length - 1] == dotBlocks[0]) {
     generateDots()
   } else {
@@ -61,6 +69,7 @@ const arrowPressed = (e) => {
               snakeBlocks.push(snakeBlocks[snakeBlocks.length - 1] - 1)
               spaces[snakeBlocks[0]].setAttribute('id', 'snake')
               spaces[snakeBlocks[0]].style.backgroundColor = '#465947'
+              spaces[snakeBlocks[0]].style.border = 'none'
               snakeBlocks.shift(snakeBlocks[0])
               previousInput.push('left')
               snake()
@@ -90,6 +99,7 @@ const arrowPressed = (e) => {
               snakeBlocks.push(snakeBlocks[snakeBlocks.length - 1] - 20)
               spaces[snakeBlocks[0]].setAttribute('id', 'snake')
               spaces[snakeBlocks[0]].style.backgroundColor = '#465947'
+              spaces[snakeBlocks[0]].style.border = 'none'
               snakeBlocks.shift(snakeBlocks[0])
               previousInput.push('top')
               snake()
@@ -118,6 +128,7 @@ const arrowPressed = (e) => {
               snakeBlocks.push(snakeBlocks[snakeBlocks.length - 1] + 1)
               spaces[snakeBlocks[0]].setAttribute('id', 'snake')
               spaces[snakeBlocks[0]].style.backgroundColor = '#465947'
+              spaces[snakeBlocks[0]].style.border = 'none'
               snakeBlocks.shift(snakeBlocks[0])
               previousInput.push('right')
               snake()
@@ -147,6 +158,7 @@ const arrowPressed = (e) => {
               snakeBlocks.push(snakeBlocks[snakeBlocks.length - 1] + 20)
               spaces[snakeBlocks[0]].setAttribute('id', 'snake')
               spaces[snakeBlocks[0]].style.backgroundColor = '#465947'
+              spaces[snakeBlocks[0]].style.border = 'none'
               snakeBlocks.shift(snakeBlocks[0])
               previousInput.push('bottom')
               snake()
@@ -169,13 +181,6 @@ const arrowPressed = (e) => {
 
 window.addEventListener('keydown', arrowPressed)
 
-const snakeEats = () => {
-  if (snakeBlocks[snakeBlocks.length] - 1 == dotBlocks[dotBlocks.length] - 1) {
-    let gainedMass = snakeBlocks[0] + 1
-    snakeBlocks.unshift(gainedMass)
-  }
-}
-
 const collisions = () => {
   if (
     (snakeBlocks[snakeBlocks.length - 1] % boardWidth == boardWidth - 1 &&
@@ -188,19 +193,17 @@ const collisions = () => {
       direction == 'bottom')
   ) {
     location.href = 'gameOver.html'
-    if (stopGame) {
-      //clear highest timeout learned from https://stackoverflow.com/questions/3847121/how-can-i-disable-all-settimeout-events
-      let highestTimeoutId = setTimeout(';')
-      for (let i = 0; i < highestTimeoutId; i++) {
-        clearTimeout(i)
-      }
-    } else {
-      return
+    //clear highest timeout learned from https://stackoverflow.com/questions/3847121/how-can-i-disable-all-settimeout-events
+    let highestTimeoutId = setTimeout(';')
+    for (let i = 0; i < highestTimeoutId; i++) {
+      clearTimeout(i)
     }
+  } else {
+    return
   }
 }
 
-const dotSpawning = () => {
+const dotEaten = () => {
   if (snakeBlocks[snakeBlocks.length - 1] == dotBlocks[0]) {
     snakeBlocks.unshift(snakeBlocks[0] + 1)
     dotBlocks.shift()
@@ -230,7 +233,7 @@ const youWin = () => {
   }
 }
 
-let dotSpawnInterval = setInterval(dotSpawning, dotSpawnTime)
+let dotSpawnInterval = setInterval(dotEaten, dotSpawnTime)
 let hitSelfCheck = setInterval(hitSelf, dotSpawnTime)
 let youWinCheck = setInterval(youWin, intervalTime)
 let interval = setInterval(collisions, intervalTime)
